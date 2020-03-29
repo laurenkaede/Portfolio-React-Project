d0cf01cc6235cb764c679b7fbb68db8df0f4932d
Lauren Moor <laurenkaede@gmail.com>
2020-03-29 16:14:02 +0100

    Updates

---
+++ precache-manifest.113c77726f059dee8a53811de358c8d0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b5dcabde68cb86e284a666eb04831fa",
+    "url": "/Portfolio-React-Project/index.html"
+  },
+  {
+    "revision": "059a81ec971af2afafd6",
+    "url": "/Portfolio-React-Project/static/css/main.92b987a7.chunk.css"
+  },
+  {
+    "revision": "c238497ac6f42b66c3bd",
+    "url": "/Portfolio-React-Project/static/js/2.0fa6ee69.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Portfolio-React-Project/static/js/2.0fa6ee69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "059a81ec971af2afafd6",
+    "url": "/Portfolio-React-Project/static/js/main.033edc2b.chunk.js"
+  },
+  {
+    "revision": "bc475ce7167a126a570e",
+    "url": "/Portfolio-React-Project/static/js/runtime-main.e7b6af5a.js"
+  },
+  {
+    "revision": "bdd3d80829bd5c52558d3f22e4fee338",
+    "url": "/Portfolio-React-Project/static/media/diceImg1.bdd3d808.jpg"
+  },
+  {
+    "revision": "a3d793bb3d518f4f8eeae44e53dc6814",
+    "url": "/Portfolio-React-Project/static/media/githubImg.a3d793bb.png"
+  },
+  {
+    "revision": "62dff00833109ec21b772952e94fc5dd",
+    "url": "/Portfolio-React-Project/static/media/img3.62dff008.jpeg"
+  },
+  {
+    "revision": "dd39145f3d337b66ffc0d542d0774768",
+    "url": "/Portfolio-React-Project/static/media/img5.dd39145f.jpg"
+  },
+  {
+    "revision": "6ed7e478420653cb5ae18e147f131db5",
+    "url": "/Portfolio-React-Project/static/media/me.6ed7e478.png"
+  },
+  {
+    "revision": "08afec0df141e04f57eb0aedd2a006f4",
+    "url": "/Portfolio-React-Project/static/media/ttImg.08afec0d.png"
+  }
+]);
